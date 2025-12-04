b0debd60b6a5199b870c30cbf2b7dd02df75dd23
alig41382 <gholamiali41380@gmail.com>
2025-12-04 22:59:12 +0330

    the rest are the meta files idc

--- Assets/Scripts/PlayerState.cs
+++ Assets/Scripts/PlayerState.cs
@@ -1,15 +1,38 @@
 using UnityEngine;
 
+/// <summary>
+/// Handles Batman's different states: Normal, Stealth, and Alert.
+/// </summary>
 public class PlayerState : MonoBehaviour
 {
-    public enum BatmanStates { Normal, Stealth, Alert }
+    public enum BatmanStates
+    {
+        Normal,
+        Stealth,
+        Alert,
+    }
 
     private BatmanStates currentState = BatmanStates.Normal;
+
+    [SerializeField]
+    AlertController alertController;
+
+    void Start()
+    {
+        currentState = BatmanStates.Normal;
+    }
 
     void Update()
     {
         HandleStateSwitch();
     }
+
+    /// <summary>
+    /// Switch between states based on key input.
+    /// C -> Stealth
+    /// Space -> Alert
+    /// Otherwise -> Normal
+    /// </summary>
     private void HandleStateSwitch()
     {
         if (Input.GetKeyDown(KeyCode.C))
@@ -24,7 +47,15 @@ public class PlayerState : MonoBehaviour
         {
             currentState = BatmanStates.Normal;
         }
+
+        if (currentState == BatmanStates.Alert)
+            alertController.SetAlert(true);
+        else
+            alertController.SetAlert(false);
     }
+    /// <summary>
+    /// Returns the current state of Batman.
+    /// </summary>
     public BatmanStates GetBatmanState()
     {
         return currentState;
